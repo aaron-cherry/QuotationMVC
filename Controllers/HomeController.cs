2c7b360632c4ee1072b88e1e8472cc0dac5ae4f6
Aaron Cherry <aaron.cherry10@gmail.com>
2023-03-20 14:15:18 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuotationMVC.Models;
+using System.Diagnostics;
+
+namespace QuotationMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Quotation quotation = new Quotation();
+            quotation.SalesPrice = 0;
+            quotation.DiscountPercent = 0;
+            return View("Quotation", quotation);
+        }
+        [HttpPost] 
+        public IActionResult Quotation(Quotation quotation)
+        {
+
+            return View("Quotation", quotation);
+        }
+    }
+}
---
+++ Models/Quotation.cs
@@ -0,0 +1,22 @@
+﻿namespace QuotationMVC.Models
+{
+    public class Quotation
+    {
+        public decimal SalesPrice { get; set; }
+        public decimal DiscountPercent { get; set; }
+        public decimal DiscountAmount
+        {
+            get
+            {
+                return SalesPrice * (DiscountPercent * .01m);
+            }
+        }
+        public decimal Total
+        {
+            get
+            {
+                return SalesPrice - DiscountAmount;
+            }
+        }
+    }
+}
